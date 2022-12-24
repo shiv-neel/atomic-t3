fbe1b4c5af08be03a119a0ddd717f6dd97c47b13
shiv-neel <shiv.neel1622@gmail.com>
2022-12-24 13:55:14 -0600

    checking if build

--- src/components/account_page_view/AppAppearance.tsx
+++ src/components/account_page_view/AppAppearance.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, useColorMode } from '@chakra-ui/react'
+import { Box, Button, useColorMode } from '@chakra-ui/react'
 import React from 'react'
 import { BsMoon, BsSun } from 'react-icons/bs'
 
@@ -7,8 +7,8 @@ const AppAppearance = () => {
 
 	return (
 		<Box>
-			<p className='text-2xl font-bold mb-8'>Application Appearance</p>
-			<Box className='flex items-center gap-12 mt-12'>
+			<p className='mb-8 text-2xl font-bold'>Application Appearance</p>
+			<Box className='mt-12 flex items-center gap-12'>
 				<p>Color Mode</p>
 				<Button
 					colorScheme='messenger'
